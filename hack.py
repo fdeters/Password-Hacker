e488fbae2d57235bd7c1f5a0673d2a55702a717b
Forrest Deters <forrest.deters@gmail.com>
2020-09-13 23:27:08 -0700

    Stage 3

--- hack.py
+++ hack.py
@@ -4,39 +4,36 @@ import string
 import itertools
 
 
-def create_guesser(password_length: int):
+def create_guesser(password_seed: str):
     """
-    Makes an iterator which generates all possible passwords (str) of a given
-    length from the set of ASCII lowercase and numerical characters.
+    Generates an iterator which supplies every possible combination of upper
+    and lower-case letters in the given word.
 
-    :param password_length: (int)
-    :returns: An iterator to be called via next() to return all possible
-        combinations. (None if password_length < 1)
+    :param password_seed: The word to be case-varied and used to guess
+        the password.
+    :returns: Iterator object, which gives a new potential password every time
+        next() is called.
     """
-    character_set = string.ascii_lowercase + '0123456789'
-
-    if password_length < 1:
-        return None
-    else:
-        return itertools.product(character_set, repeat=password_length)
+    s = password_seed
+    return map(lambda x: ''.join(x),
+               itertools.product(*zip(s.lower(), s.upper())))
 
 
-def guess_all(guess_iterator, socket_object):
+def guess(guess_iterator, socket_object):
     """
-    Uses a provided iterator to attempt to guess the password. Halts guessing
-    if the server returns a success or a lockout.
+    Guesses all possible permutations of the word currently being guessed.
 
-    :param guess_iterator: An iterator object which returns a password guess
-        (str) with every application of next().
-    :param socket_object: A connected socket object, used to pass messages to
-        the server.
+    :param guess_iterator: An iterator which yields all possible combinations
+        of upper & lower-case letters in the word currently being guessed.
+    :param socket_object: A socket object connected to the server socket. Used
+        to communicate with the server.
     :return: If the password was guessed correctly returns the guess (str),
         None if no attempts succeeded, 'LOCKOUT' (str) if the server locked us
         out.
     """
     while True:
         try:
-            guess = ''.join(next(guess_iterator))
+            guess = next(guess_iterator)
 
             socket_object.send(bytes(guess, encoding='utf-8'))
             reply = socket_object.recv(1024).decode()
@@ -54,6 +51,8 @@ def main():
     """
     Main function for the hacker program.
     """
+    word_file_name = 'passwords.txt'
+
     # get arguments from CL and type-wrangle
     _file, server_ip, server_port = sys.argv
     server_port = int(server_port)
@@ -62,18 +61,18 @@ def main():
     with socket.socket() as sock:
         sock.connect((server_ip, server_port))
 
-        guess_length = 1
-        while True:
-            guesser = create_guesser(guess_length)
-            result = guess_all(guesser, sock)
+        with open(word_file_name, 'r') as f:
 
-            if result == 'LOCKOUT':
-                break
-            elif result is not None:  # this means success!
-                print(result)
-                break
-            else:
-                guess_length += 1
+            while True:
+                common_password = f.readline().strip()
+                guesser = create_guesser(common_password)
+                result = guess(guesser, sock)
+
+                if result == 'LOCKOUT':
+                    break
+                elif result is not None:  # this means success!
+                    print(result)
+                    break
 
 
 if __name__ == '__main__':
